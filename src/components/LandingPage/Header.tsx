93d5782f6299cbbcc1b02f04b784090c531e1881
alequissandrara <alequissandrarayane123@gmail.com>
2023-11-23 15:17:15 -0300

    Criação do Login e do useForms

--- src/components/LandingPage/Header.tsx
+++ src/components/LandingPage/Header.tsx
@@ -2,26 +2,36 @@ import LogoResearchhub from "../../assets/logo-research-hub.png";
 import Image from "next/image";
 import { Button, Flex, Heading, Link } from "@chakra-ui/react";
 
-export default function Header() {
+interface HeaderProps {
+  showLinksAndButton?: boolean;
+}
+
+export default function Header({ showLinksAndButton = true }: HeaderProps) {
   return (
     <Flex h="10vh" justifyContent="space-around" alignItems="center" p={4}>
       <Flex alignItems="center">
         <Image src={LogoResearchhub} alt="" width={40} height={40} />
-        <Heading size="md">Research<span style={{color: '#FF8500'}}>Hub</span></Heading>
+        <Heading size="md">
+          Research<span style={{ color: "#FF8500" }}>Hub</span>
+        </Heading>
       </Flex>
-      <Flex gap="40px" display={{base: 'none', md: 'flex'}}>
-        <Link>Home</Link>
-        <Link>Projetos</Link>
-        <Link>Perfil</Link>
-      </Flex>
-      <Button
-        color="white"
-        bg="#5A189A"
-        borderRadius="8px"
-        _hover={{ opacity: 0.8}}
-      >
-        Login / Cadastro
-      </Button>
+      {showLinksAndButton && (
+        <Flex gap="40px" display={{ base: "none", md: "flex" }}>
+          <Link>Home</Link>
+          <Link>Projetos</Link>
+          <Link>Perfil</Link>
+        </Flex>
+      )}
+      {showLinksAndButton && (
+        <Button
+          color="white"
+          bg="#5A189A"
+          borderRadius="8px"
+          _hover={{ opacity: 0.8 }}
+        >
+          Login / Cadastro
+        </Button>
+      )}
     </Flex>
   );
 }
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,10 +1,120 @@
-import React from 'react'
+import Header from "@/src/components/LandingPage/Header";
+import React from "react";
+import {
+  Flex,
+  Box,
+  Image,
+  FormControl,
+  FormLabel,
+  Input,
+  Text,
+  Link,
+  SimpleGrid,
+  Button,
+} from "@chakra-ui/react";
+import peopleSearch from "../../assets/people-search-amico.svg";
+import useCustomForm from "./useForm";
 
-export default function Login(){
+const formFields = [
+  {
+    id: "email",
+    label: "Email",
+    type: "email",
+    placeholder: "user@exemplo.com",
+  },
+  {
+    id: "senha",
+    label: "Senha",
+    type: "password",
+    placeholder: "**********",
+  },
+];
+
+export default function Login() {
+  const emailProps = useCustomForm({ type: "email" });
+  const passwordProps = useCustomForm({ type: "password" });
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const emailValue = emailProps.value;
+    const passwordValue = passwordProps.value;
+
+    console.log("Email:", emailValue);
+    console.log("Senha:", passwordValue);
+  };
+
   return (
-    <div>
-      login
-    </div>
-  )
+    <>
+      <Flex alignItems="left">
+        <Header showLinksAndButton={false} />
+      </Flex>
+      <SimpleGrid spacing={5} templateColumns="repeat(2, minmax(200px, 1fr))">
+        <Box>
+          <Image
+            src={peopleSearch.src}
+            alt="People Search"
+            width={800}
+            height={800}
+          />
+        </Box>
+        <Box justifyContent="center" alignItems="center">
+          <Text fontWeight={700} fontSize={60} mb="2rem">
+            Login
+          </Text>
+          <form onSubmit={handleSubmit}>
+            {formFields.map(({ id, label, type, placeholder }) => (
+              <FormControl key={id} ml="1rem">
+                <FormLabel mt="1rem" htmlFor={id}>
+                  {label}
+                </FormLabel>
+                <Input
+                  size="lg"
+                  w="70%"
+                  id={id}
+                  type={type}
+                  value={
+                    type === "email" ? emailProps.value : passwordProps.value
+                  }
+                  onChange={
+                    type === "email"
+                      ? emailProps.onChange
+                      : passwordProps.onChange
+                  }
+                  onBlur={
+                    type === "email" ? emailProps.onBlur : passwordProps.onBlur
+                  }
+                  focusBorderColor="orange.400"
+                  placeholder={placeholder}
+                />
+                {type === "email" && emailProps.error && (
+                  <Text color="red.500">{emailProps.error}</Text>
+                )}
+                {type === "password" && passwordProps.error && (
+                  <Text w="65%" color="red.500">
+                    {passwordProps.error}
+                  </Text>
+                )}
+              </FormControl>
+            ))}
+            <Text mt=".5rem" ml="1rem" fontWeight={600}>
+              Não tem conta?
+              <Link style={{ color: "#5A189A" }}> Cadastrar</Link>
+            </Text>
+            <Button
+              type="submit"
+              width="55%"
+              size="lg"
+              m={55}
+              bg="#FF8500"
+              borderRadius="8px"
+              _hover={{ opacity: 0.8 }}
+            >
+              Entrar
+            </Button>
+          </form>
+        </Box>
+      </SimpleGrid>
+    </>
+  );
 }
-
