af786887c56af442770e6358c9bdec9ae47e423f
alequissandrara <alequissandrarayane123@gmail.com>
2023-12-01 16:37:22 -0300

    Criação do forms de inscrição

---
+++ src/pages/singUp/index.tsx
@@ -0,0 +1,91 @@
+import {
+  Flex,
+  Box,
+  Image,
+  FormControl,
+  FormLabel,
+  Input,
+  Text,
+  Link,
+  SimpleGrid,
+  Button,
+} from "@chakra-ui/react";
+import Header from "@/src/components/LandingPage/Header";
+import imgNotebook from "../../assets/imgNotebook.png";
+import formFieldsSingUp from "./data";
+
+export default function singUp() {
+  return (
+    <>
+      <Header showLink={true} showButton={false} showUser={true} />
+      <SimpleGrid spacing={5} templateColumns="repeat(2, minmax(200px, 1fr))">
+        <Box
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Text fontWeight={700} fontSize={60} textAlign="center">
+            Inscreva-se
+          </Text>
+
+          <form /* onSubmit={handleSubmit} */>
+            {formFieldsSingUp.map(({ id, label, type, placeholder }) => (
+              <FormControl
+                key={id}
+                ml="1rem"
+                justifyContent="center"
+                alignItems="center"
+              >
+                <FormLabel mt="1rem" htmlFor={id}>
+                  {label}
+                </FormLabel>
+                <Input
+                  size="lg"
+                  w="100%"
+                  id={id}
+                  type={type}
+                  placeholder={placeholder}
+                  boxShadow="md"
+                  focusBorderColor="#FF8500"
+                />
+              </FormControl>
+            ))}
+            <Input
+              mt={5}
+              ml={4}
+              size="lg"
+              w="50%"
+              boxShadow="md"
+              type="file"
+              onChange={(e) => {
+                const selectedFile = e.target.files?.[0];
+                console.log(selectedFile);
+              }}
+            />
+            <Button
+              type="submit"
+              width="35%"
+              size="lg"
+              m={55}
+              bg="#FF8500"
+              borderRadius="8px"
+              boxShadow="md"
+              _hover={{ opacity: 0.8 }}
+            >
+              Inscrever
+            </Button>
+          </form>
+        </Box>
+        <Box>
+          <Image
+            src={imgNotebook.src}
+            alt="People Search"
+            width="auto"
+            height="90vh"
+          />
+        </Box>
+      </SimpleGrid>
+    </>
+  );
+}
